6dd02696ace663c3892f05be3baff7d5ab6a860d
liyun-li <liyun-li@users.noreply.github.com>
2019-03-10 17:15:21 -0400

    Add skeleton JavaScript and Bootstrapt CDN

--- server/app/views.py
+++ server/app/views.py
@@ -1,16 +1,40 @@
-from flask import (
-    request, json, make_response, redirect,
-    url_for, session, Blueprint, render_template
-)
+from flask import request, json, make_response, redirect, url_for, session, \
+    Blueprint, render_template
+
+from app.models import db, User
 
 
 views = Blueprint('views', __name__)
 
 
+def safer_commit(session):
+    try:
+        session.commit()
+        return True
+    except:
+        session.rollback()
+        raise
+    finally:
+        return False
+
+
 @views.route('/', methods=['GET'])
 def index():
     open_id = request.args.get('open_id')
-    return render_template('index.html')
+    if open_id:
+        user = User.query.filter_by(open_id=open_id).first()
+        if not user:
+            user = User(open_id=open_id, gender=gender)
+            db.session.add(user)
+
+            if not safer_commit(db.session):
+                error = 'Something went wong. Please contact staff.'
+                return render_template('index.html', error=error)
+
+        return render_template('index.html')
+
+    error = 'You must view this page with WeChat.'
+    return render_template('index.html', error=error)
 
 
 @views.route('/tutorial', methods=['GET'])
