fcfe823bdb076b1d8ca9ac1211b54013da4f625f
liyun-li <liyun-li@users.noreply.github.com>
2019-03-24 17:55:01 -0400

    {WIP} more improvement

--- server/app/models.py
+++ server/app/models.py
@@ -1,28 +1,43 @@
-from sqlalchemy import Column, String, CheckConstraint as CC
+from sqlalchemy import Column, String, \
+    ForeignKey as FK, CheckConstraint as CC
 from flask_sqlalchemy import SQLAlchemy
 import enum
 
 db = SQLAlchemy()
 
+
 class UserStatus(enum.Enum):
-    NonSex = "sex not selected"
-    Waiting = "waiting in the queue"
-    Assigned = "already assigned a number"
+    NonSex = "Gender not selected"
+    Waiting = "Waiting in the queue"
+    Assigned = "Already assigned a number"
+
 
 class User(db.Model):
     __tablename__ = 'user'
 
     open_id = Column(String(255), primary_key=True)
-    # own gender
+    # gender
     gender = Column(String(1))
-    # liked gender
-    like_gender = Column(String(1))
-
+    # preference
+    preference = Column(String(1))
+    # refer to `UserStatus`
     status = Column(db.Enum(UserStatus))
-
-    # number
-    number = Column(db.Integer)
 
     __table_args__ = (
         CC('gender = "M" or gender = "F"'),
+        CC('preference = "M" or preference = "F"')
     )
+
+
+class Waitlist(db.Model):
+    __tablename__ = 'waitlist'
+
+    open_id = Column(String(255), FK('user.open_id'), primary_key=True)
+
+
+class Matches(db.Model):
+    __tablename__ = 'matches'
+
+    group_id = Column(String(4), primary_key=True)
+    user_1 = Column(String(255), FK('user.open_id'))
+    user_2 = Column(String(255), FK('user.open_id'))
