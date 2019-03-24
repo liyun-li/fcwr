327c7f9d4f981410aa6863b7476b2f45cea87b59
Kai Chen <kchen9530@gmail.com>
2019-03-24 15:58:14 -0400

    Add columns in table

--- server/serve.py
+++ server/serve.py
@@ -2,4 +2,8 @@ if __name__ == '__main__':
     from app import create_app
 
     app = create_app()
+
+    # secret key for session
+    app.secret_key = 'super secret key'
+
     app.run(host='0.0.0.0', port=3000)
