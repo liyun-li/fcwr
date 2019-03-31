8731235554965d4cba81ca727edf5106e027cf29
Kaixuan Zhou <zhoukx.joseph@gmail.com>
2019-03-31 15:40:36 -0400

    frontend index.html

--- server/app/static/js/index.js
+++ server/app/static/js/index.js
@@ -5,3 +5,51 @@ $('#waitlist').ready(() => {
 		$('#waitlist').text(data);
 	});
 });
+
+
+$(document).ready(function() {
+	var self_gender;
+	var prefer_gender;
+
+	[$("#self-female"),$("#self-male"),$("#prefer-male"),$("#prefer-female")].forEach(function(elem){
+		elem.css("opacity",".5");
+	});
+
+	$("#self-female").on('click',function(){
+		$("#self-female").css("opacity","1");
+		$("#self-male").css("opacity",".5");
+		self_gender = "F";
+	});
+	$("#self-male").on('click',function(){
+		$("#self-male").css("opacity","1");
+		$("#self-female").css("opacity",".5");
+		self_gender = "M";
+	});
+	$("#prefer-male").on('click',function(){
+		$("#prefer-male").css("opacity","1");
+		$("#prefer-female").css("opacity",".5");
+		prefer_gender = "M";
+	});
+	$("#prefer-female").on('click',function(){
+		$("#prefer-male").css("opacity",".5");
+		$("#prefer-female").css("opacity","1");
+		prefer_gender = "F";
+	});
+
+	$("#submit-btn").on('click',function(){
+		if(prefer_gender && self_gender){
+			$.ajax({
+				type: "POST",
+				url: "/setSexAndInterest?sex="+self_gender+"?interest="+prefer_gender,
+				success:function(res){
+					location.reload();
+				},
+				error:function(err){
+					alert("err set sex" + err);
+				},
+	    	});
+		}else{
+			alert("you must select your gender and interest");
+		}
+	});
+});
